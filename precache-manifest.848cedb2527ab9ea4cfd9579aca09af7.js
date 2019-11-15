b6af13d002e0b4411e09cc5ddba9ec81af3e68df
fariisiibrahiim <farisibrahmi@gmail.com>
2019-11-15 16:03:07 +0700

    Updates

---
+++ precache-manifest.848cedb2527ab9ea4cfd9579aca09af7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cd9063337ad9a39150f772658b7d4f",
+    "url": "/learn-react-baim/index.html"
+  },
+  {
+    "revision": "4a0dc7abb7099df078a2",
+    "url": "/learn-react-baim/static/css/main.60808692.chunk.css"
+  },
+  {
+    "revision": "80bfffac9c9eee38e212",
+    "url": "/learn-react-baim/static/js/2.41fe28f7.chunk.js"
+  },
+  {
+    "revision": "4a0dc7abb7099df078a2",
+    "url": "/learn-react-baim/static/js/main.ec180960.chunk.js"
+  },
+  {
+    "revision": "a127e114e4afbad22da4",
+    "url": "/learn-react-baim/static/js/runtime-main.4d4d0c10.js"
+  },
+  {
+    "revision": "d3ecca805dcdbad436c78fa9b04e32c7",
+    "url": "/learn-react-baim/static/media/cleanMedallion.d3ecca80.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/learn-react-baim/static/media/logo.25bf045c.svg"
+  }
+]);
